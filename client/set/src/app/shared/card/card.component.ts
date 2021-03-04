78a984bd4565868a4f58f2d4a730afef02413a3c
ckVendrix <clk.redox@gmail.com>
2021-03-04 20:05:53 +0100

    Card component

--- client/set/src/app/shared/card/card.component.ts
+++ client/set/src/app/shared/card/card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { setCard } from 'src/app/interface/set-card';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() card: setCard = {
+    id: 1,
+    properties: {
+      color: 'round',
+      shape: 'red',
+      amount: 1,
+      filling: 'striped',
+    },
+  };
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
